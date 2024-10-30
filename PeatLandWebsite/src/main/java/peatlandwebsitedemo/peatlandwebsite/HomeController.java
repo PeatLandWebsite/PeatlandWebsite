864507b9751259592eb63828e7bf76d106f74f14
Chengjie Diao <cdiao31@gatech.edu>
2024-10-30 17:37:42 -0600

    new udpate

--- PeatLandWebsite/src/main/java/peatlandwebsitedemo/peatlandwebsite/HomeController.java
+++ PeatLandWebsite/src/main/java/peatlandwebsitedemo/peatlandwebsite/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         // Add the message to the model to display it on the view
-        model.addAttribute("message", "Forecasting Human Footprint and Peatland Disturbance");
+        model.addAttribute("message", "The Peatland Disturbance and Human Footprint Research Project website is dedicated to studying the impacts of human activities on peatlands, focusing on developing advanced machine learning models to forecast peatland disturbances. By leveraging datasets from the Alberta Biodiversity Monitoring Institute (ABMI) and incorporating human footprint data, the project aims to build accurate models that can capture complex spatial and temporal dynamics. This research explores diverse human-induced factors, such as industrial activities, agricultural land expansion, and infrastructure development, which contribute to peatland degradation. Utilizing modern machine learning techniques like CNNs, LSTMs, and Transformers, along with high-performance computing, the project seeks to provide data-driven insights and predictive tools to inform conservation policies, manage ecological impacts, and support sustainable land use in Alberta and beyond.");
         return "home";  // Return the view name (index.html)
     }
 }
